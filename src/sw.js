9828921b460b507e689533b3a0c381530a0af1cc
marmooo <marmooo@users.noreply.github.com>
2022-11-23 15:26:58 +0900

    add Service Worker

---
+++ src/sw.js
@@ -0,0 +1,47 @@
+var CACHE_NAME = "2022-11-23 15:30";
+var urlsToCache = [
+  "/midi2abc/",
+  "/midi2abc/index.js",
+  "/midi2abc/midi2abc.js",
+  "/midi2abc/cooleys.mid",
+  "/midi2abc/favicon/favicon.svg",
+  "https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css",
+  "https://cdn.jsdelivr.net/combine/npm/tone@14.7.77,npm/@magenta/music@1.23.1/es6/core.js,npm/abcjs@6.1.7/dist/abcjs-basic-min.js",
+];
+
+self.addEventListener("install", function (event) {
+  event.waitUntil(
+    caches
+      .open(CACHE_NAME)
+      .then(function (cache) {
+        return cache.addAll(urlsToCache);
+      }),
+  );
+});
+
+self.addEventListener("fetch", function (event) {
+  event.respondWith(
+    caches.match(event.request)
+      .then(function (response) {
+        if (response) {
+          return response;
+        }
+        return fetch(event.request);
+      }),
+  );
+});
+
+self.addEventListener("activate", function (event) {
+  var cacheWhitelist = [CACHE_NAME];
+  event.waitUntil(
+    caches.keys().then(function (cacheNames) {
+      return Promise.all(
+        cacheNames.map(function (cacheName) {
+          if (cacheWhitelist.indexOf(cacheName) === -1) {
+            return caches.delete(cacheName);
+          }
+        }),
+      );
+    }),
+  );
+});

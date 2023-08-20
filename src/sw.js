30a789032958af97dae2cd4bdd151e1049e434f1
marmooo <marmooo@users.noreply.github.com>
2023-08-21 08:10:27 +0900

    cleanup

--- src/sw.js
+++ src/sw.js
@@ -1,5 +1,5 @@
-var CACHE_NAME = "2023-07-29 10:17";
-var urlsToCache = [
+const CACHE_NAME = "2023-08-21 08:10";
+const urlsToCache = [
   "/midi2abc/",
   "/midi2abc/index.js",
   "/midi2abc/midi2abc.js",
@@ -8,38 +8,28 @@ var urlsToCache = [
   "https://cdn.jsdelivr.net/combine/npm/tone@14.7.77,npm/@magenta/music@1.23.1/es6/core.js,npm/abcjs@6.2.2/dist/abcjs-basic-min.js",
 ];
 
-self.addEventListener("install", function (event) {
+self.addEventListener("install", (event) => {
   event.waitUntil(
-    caches
-      .open(CACHE_NAME)
-      .then(function (cache) {
-        return cache.addAll(urlsToCache);
-      }),
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(urlsToCache);
+    }),
   );
 });
 
-self.addEventListener("fetch", function (event) {
+self.addEventListener("fetch", (event) => {
   event.respondWith(
-    caches.match(event.request)
-      .then(function (response) {
-        if (response) {
-          return response;
-        }
-        return fetch(event.request);
-      }),
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    }),
   );
 });
 
-self.addEventListener("activate", function (event) {
-  var cacheWhitelist = [CACHE_NAME];
+self.addEventListener("activate", (event) => {
   event.waitUntil(
-    caches.keys().then(function (cacheNames) {
+    caches.keys().then((cacheNames) => {
       return Promise.all(
-        cacheNames.map(function (cacheName) {
-          if (cacheWhitelist.indexOf(cacheName) === -1) {
-            return caches.delete(cacheName);
-          }
-        }),
+        cacheNames.filter((cacheName) => cacheName !== CACHE_NAME)
+          .map((cacheName) => caches.delete(cacheName)),
       );
     }),
   );
